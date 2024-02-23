3a1c3d4175761d73a73eadd28da3671492bacf4c
ayhanma <ayhanorazov2008@gmail.com>
2024-02-23 16:18:09 +0500

    initial commit

---
+++ python.py
@@ -0,0 +1,31 @@
+# def factorial(num):
+#     sum = 1
+#     for i in range(2,num+1):
+#        sum*=i
+#     print(sum)
+# print(factorial(5))
+# list = [3,2,1,0]
+# for i in range(0,len(list)):
+#     print(list[i])
+# guess_me = 7
+# number = 1
+# while True:
+#     if number>guess_me:
+#         print('too low')
+#         guess_me+=1
+#     elif number<guess_me:
+#         print('ooops')
+#         guess_me -=1
+#     elif number == guess_me:
+#         print('got it')
+#         break
+# guess_me = 5
+# for number in range(1,10):
+#     if number>guess_me:
+#         print('too low')
+#         guess_me+=1
+#     elif number<guess_me:
+#         print('ooops')
+#         guess_me -=1
+#     elif number == guess_me:
+#         print('got it')
